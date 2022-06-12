41de5b8cebe42775e3dfd77b91762bb3e8ed6650
Gautier DI FOLCO <gautier.difolco@gmail.com>
2022-06-12 14:48:43 +0200

    Migrate to nix flakes

--- default.nix
+++
@@ -1,2 +0,0 @@
-{ nixpkgs ? import ./nixpkgs.nix { }, compiler ? "ghc8107" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./nonempty.nix { }
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "nonempty";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskell.packages.ghc922;
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        packageName = "nonempty";
+      in
+      {
+        packages.${packageName} = # (ref:haskell-package-def)
+          haskellPackages.callCabal2nix packageName self rec {
+            # Dependency overrides go here
+          };
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with haskellPackages; [
+            haskell-language-server
+            ghcid
+            cabal-install
+          ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
